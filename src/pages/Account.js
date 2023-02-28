332146045b396ea9bc66c99b4cec15a685c66a3f
flashkard <sempre15@students.rowan.edu>
2023-02-28 09:44:37 -0500

    Main UI changes

---
+++ src/pages/Account.js
@@ -0,0 +1,10 @@
+export function Account() {
+    return (
+        <div>
+            <div className="header-container">
+                <div className="Title">Account</div>
+            </div>
+            <div className="body-container"></div>
+        </div>
+    )
+}
---
+++ src/pages/TabsLinks.js
@@ -0,0 +1,10 @@
+export function TabsLinks() {
+    return (
+        <div>
+            <div className="header-container">
+                <div className="Title">Tabs and Links</div>
+            </div>
+            <div className="body-container"></div>
+        </div>
+    )
+}
