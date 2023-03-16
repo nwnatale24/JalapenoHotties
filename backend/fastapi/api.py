9ebf6cc99ff52f60b59f6609a388c8ff04ff0a43
Nick Natale <74843836+nwnatale24@users.noreply.github.com>
2023-03-16 17:21:13 -0400

    Create get user endpoing using user id and full name (#11)
    * added endpoint for getting info about a user by providing user_id.
    
    * add get user information by full name endpoint. fixed path of other endpoint url. fix restaurant endpoint payload data.

--- backend/fastapi/api.py
+++ backend/fastapi/api.py
@@ -42,6 +42,19 @@ review_table = Table(
     Column("restaurant_id", Integer, nullable=False)
     )
     
+
+# Create a Table object that represents the "restaurant" table in the DB.
+user_table = Table(
+    'user', 
+    metadata, 
+    Column("user_id", Integer, primary_key=True),
+    Column("first_name", String(45), nullable=True),
+    Column("last_name", String(45), nullable=True), 
+    Column("email_address", String(45), nullable=True),
+    Column("user_rank", String(45), nullable=True),
+    Column("user_image", String(45), nullable=True) # TODO: Need to change from String to access image ...
+    )
+
 metadata.create_all(engine)
 
 # Endpoint for GET request to return all reviews about a particular 
@@ -97,7 +110,7 @@ async def get_review_by_restaurant_id(rest_id):
 
 
 # Endpoint for GET request to return JSON of all restaurants and their attributes.
-@app.get("/api/restaurants")
+@app.get("/api/restaurants/")
 async def get_all_resurants():
     try:
         # Open a conection to the DB.
@@ -213,3 +226,103 @@ async def post_restaurant( city: str, name: str, website: str, phone_number: str
     # If not successful, return an error with the error message. 
     except Exception as e:
         return {"state" : 'error', "message" : str(e)}
+
+
+# Endpoint for GET request to return JSON of user and it's attributes, searched by user_id.
+@app.get("/api/users/id/{user_id}") 
+async def get_user_by_user_id(user_id):
+    try:
+
+        # Open a conection to the DB.
+        conn = engine.connect()
+
+        # Execute a SELECT query on the "restaurant" table
+        select_query = user_table.select().where(user_table.c.user_id == user_id)
+        results = conn.execute(select_query)
+
+        # Header for the matches dict.
+        matches = {"status" : "success",
+                    "message" : "null",
+                    "users" : []}
+                    
+        # Create an array of dicts containing the results.
+        for row in results:
+            user_id = row[0]
+            first_name = row[1]
+            last_name= row[2]
+            email_address = row[3]
+            user_rank = row[4]
+            user_image = row[5]
+
+            matches["users"].append({
+                                "id" : user_id,
+                                "first_name" : first_name,
+                                "last_name" : last_name,
+                                "email_address" : email_address,
+                                "user_rank" : user_rank,
+                                "user_image" : user_image
+                
+             })
+
+        # Close the connection to the DB. 
+        conn.close()
+
+        return(matches)
+        
+    except Exception as e:
+        return {"status" : "fail",
+                "message" : str(e)}
+    
+# Endpoint for GET request to return JSON of user and it's attributes, searched by user_id.
+@app.get("/api/users/full_name/{full_name}") 
+async def get_user_by_full_name(full_name):
+    try:
+
+        # Open a conection to the DB.
+        conn = engine.connect()
+
+        # Split full name provided in path into first name and last name. 
+        first_and_last_name_array = full_name.split(" ")
+
+        if len(first_and_last_name_array) != 2:
+            raise Exception("Full name must consist of only a first name and last name, separated by one space.")
+
+        first_name = first_and_last_name_array[0]
+        last_name = first_and_last_name_array[1]
+
+        # Execute a SELECT query on the "restaurant" table
+        select_query = user_table.select().where(user_table.c.first_name == first_name).where(user_table.c.last_name == last_name)
+        results = conn.execute(select_query)
+
+        # Header for the matches dict.
+        matches = {"status" : "success",
+                    "message" : "null",
+                    "users" : []}
+                    
+        # Create an array of dicts containing the results.
+        for row in results:
+            user_id = row[0]
+            first_name = row[1]
+            last_name= row[2]
+            email_address = row[3]
+            user_rank = row[4]
+            user_image = row[5]
+
+            matches["users"].append({
+                                "id" : user_id,
+                                "first_name" : first_name,
+                                "last_name" : last_name,
+                                "email_address" : email_address,
+                                "user_rank" : user_rank,
+                                "user_image" : user_image
+                
+             })
+
+        # Close the connection to the DB. 
+        conn.close()
+
+        return(matches)
+        
+    except Exception as e:
+        return {"status" : "fail",
+                "message" : str(e)}
