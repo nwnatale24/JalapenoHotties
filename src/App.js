da1529b404102a5a5db1c930c2f49364b24e134d
flashkard <116611881+flashkard@users.noreply.github.com>
2023-03-26 15:44:47 -0400

    *Added Review button for Resturaunts and Review pg (#15)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import { Account } from "./pages/Account"
 import { Home } from "./pages/Home"
+import { Reviews } from "./pages/Reviews"
 import { TabsLinks } from "./pages/TabsLinks"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Route path="/" element={<Home />} />
     <Route path="/Account" element={<Account />} />
     <Route path="/TabsLinks" element={<TabsLinks />} />
+    <Route path="/Reviews" element={<Reviews />} />
   </Routes>
 }
 
---
+++ src/pages/Reviews.js
@@ -0,0 +1,10 @@
+export function Reviews() {
+    return (
+        <div>
+            <div className="header-container">
+                <div className="Title"><i>INSERT RESTURAUNT NAME</i> Reviews</div>
+            </div>
+            <div className="body-container"></div>
+        </div>
+    )
+}
