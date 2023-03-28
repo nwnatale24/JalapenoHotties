3a017a3f0a383b5011e28e88297b65b450901cb6
flashkard <116611881+flashkard@users.noreply.github.com>
2023-03-28 19:14:11 -0400

    List reviews lor (#19)
    * *Bugifx : Restaurants with no reviews now update.
    
    * *All Reviews passed to component instead of one.

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -3,12 +3,11 @@ import '../App.css';
 import { Link } from "react-router-dom"
 import { useState } from 'react';
 function Review(props){
-
 return (
     <div className='review-body'>
-           <p><b>Title:</b>  {props.title}</p>
-           <p>{props.text}</p>
-           <p><b>Score:</b>  {props.score}</p>
+           <p><b>Title:</b>  {props.reviews[1][1]}</p>
+           <p><b>Text:</b>{props.reviews[1][2]}</p>
+           <p><b>Score:</b>  {props.reviews[1][3]}</p>
     </div>  
 )
 }
