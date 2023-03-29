c5c37a274233c972e02b0578deef0c824717f56c
flashkard <sempre15@students.rowan.edu>
2023-03-28 21:11:02 -0400

    *Fixed format of review props.

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -3,13 +3,13 @@ import '../App.css';
 import { Link } from "react-router-dom"
 import { useState } from 'react';
 function Review(props){
+    
 return (
     <div className='review-body'>
-           <p><b>Title:</b>  {props.reviews[1][1]}</p>
-           <p><b>Text:</b>{props.reviews[1][2]}</p>
-           <p><b>Score:</b>  {props.reviews[1][3]}</p>
+           <p><b>Title:</b>  {props.reviews[0][1]}</p>
+           <p><b>Text:</b>{props.reviews[0][2]}</p>
+           <p><b>Score:</b>  {props.reviews[0][3]}</p>
     </div>  
-)
-}
+)}
   
 export default Review;
