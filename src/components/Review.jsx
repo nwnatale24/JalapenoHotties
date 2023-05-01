be26f29ecb50e66495ab2f5ee0a07c2b4764fa01
Nick Natale <74843836+nwnatale24@users.noreply.github.com>
2023-05-01 14:04:18 -0400

    Revert "Code clean up (#39)" (#40)
    This reverts commit 13e734d16a51dfd9695f39b1204907002698e4d2 from PR #39.

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,61 +1,101 @@
-import React, { useState } from 'react';
+import React from 'react'
 import '../App.css';
-import { Link } from "react-router-dom";
-
+import { Link } from "react-router-dom"
+import { useState } from 'react';
 function Review(props){
-  const [isHighLow, setHighLow] = useState(false);
-  const [isLowHigh, setLowHigh] = useState(false);
-  const [isUnsorted, setUnsorted] = useState(false);
-  const [isRecent, setRecent] = useState(false);
-  const [isOld, setOld] = useState(false);
-  
-  const all_reviews = props.reviews.map(review => ({
-    title: review[1],
-    desc: review[2],
-    score: review[3],
-    timestamp: review[5]
-  }));
+    const [isHighLow, setHighLow] = useState(false)
+    const [isLowHigh, setLowHigh] = useState(false)
+    const [isUnsorted, setUnsorted] = useState(false)
+    const [isRecent, setRecent] = useState(false)
+    const [isOld, setOld] = useState(false)
+    
+    const all_reviews = []
+    for (let i = 0 ; i < props.reviews.length ; i++){
+        all_reviews[i] = {
+            title : props.reviews[i][1],
+            desc : props.reviews[i][2],
+            score : props.reviews[i][3],
+            timestamp: props.reviews[i][5]
+        }
+    }
 
-  /* toSorted() is used instead of sort() because sort() mutates
-  the original array every time it is invoked which was causing
-  a bug where low_high reviews wouldn't work. toSorted() allows
-  you to use a shallow copy and not mutate the original array.*/
+    /* toSorted() is used instead of sort() because sort() mutates
+    the original array every time it is invoked which was causing
+    a bug where low_high reviews wouldn't work. toSorted() allows
+    you to use a shallow copy and not mutate the original array.*/
 
-  const toSorted = (arr, compareFn) => [...arr].sort(compareFn);
-
-  const low_high = toSorted(all_reviews, (a, b) => a.score - b.score);
-  const high_low = toSorted(all_reviews, (a, b) => b.score - a.score);
-  const recent = toSorted(all_reviews, (a,b) => b.timestamp - a.timestamp)
-  const old = toSorted(all_reviews, (a,b) => a.timestamp - b.timestamp)
-  
-  const reviewList = (reviews) => (
-    reviews.map(review => (
-      <div className='review' key={review.timestamp}>
-        <p><b>Title:</b> {review.title}</p>
-        <p><b>Text:</b> {review.desc}</p>
-        <p><b>Score:</b> {review.score}</p>
-        <p>{review.timestamp}</p>
-      </div>
+    const low_high = all_reviews.toSorted((a, b) => a.score - b.score);
+    const high_low = all_reviews.toSorted((a, b) => b.score - a.score);
+    const recent = all_reviews.toSorted((a,b) => b.timestamp - a.timestamp)
+    const old = all_reviews.toSorted((a,b) => a.timestamp - b.timestamp)
+    
+    const unsorted_list = all_reviews.map(review_1 => (
+        <div className='review'>
+           <p><b>Title:</b>  {review_1.title}</p>
+           <p><b>Text:</b>{review_1.desc}</p>
+           <p><b>Score:</b>  {review_1.score}</p>
+           <p>{review_1.timestamp}</p>
+        </div>
     ))
-  );
-
-  return (
+    const high_low_list = high_low.map(review_2 => (
+        <div className='review'>
+           <p><b>Title:</b>  {review_2.title}</p>
+           <p><b>Text:</b>{review_2.desc}</p>
+           <p><b>Score:</b>  {review_2.score}</p>
+           <p>{review_2.timestamp}</p>
+        </div>
+    ))
+    const low_high_list = low_high.map(review_3 => (
+        <div className='review'>
+           <p><b>Title:</b>  {review_3.title}</p>
+           <p><b>Text:</b>{review_3.desc}</p>
+           <p><b>Score:</b>  {review_3.score}</p>
+           <p>{review_3.timestamp}</p>
+        </div>
+    ))
+    const recent_list = recent.map(review_4 => (
+        <div className='review'>
+           <p><b>Title:</b>  {review_4.title}</p>
+           <p><b>Text:</b>{review_4.desc}</p>
+           <p><b>Score:</b>  {review_4.score}</p>
+           <p>{review_4.timestamp}</p>
+        </div>
+    ))
+    const old_list = old.map(review_5 => (
+        <div className='review'>
+           <p><b>Title:</b>  {review_5.title}</p>
+           <p><b>Text:</b>{review_5.desc}</p>
+           <p><b>Score:</b>  {review_5.score}</p>
+           <p>{review_5.timestamp}</p>
+        </div>
+    ))
+    
+return (
     <div className='review-body'>
-      <div className='button-body'>
+        <div className='button-body'>
         <button className='review-sort-buttons' onClick={() => {setUnsorted(!isUnsorted);setHighLow(false);setLowHigh(false);setRecent(false);setOld(false)}}>Unordered</button>
         <button className='review-sort-buttons' onClick={() => {setHighLow(!isHighLow);setUnsorted(false);setLowHigh(false);setRecent(false);setOld(false)}}>Best-Worst Scores</button>
         <button className='review-sort-buttons' onClick={() => {setLowHigh(!isLowHigh);setUnsorted(false);setHighLow(false);setRecent(false);setOld(false)}}>Worst-Best Scores</button>
         <button className='review-sort-buttons' onClick={() => {setRecent(!isRecent);setUnsorted(false);setHighLow(false);setLowHigh(false);setOld(false)}}>Recent-Oldest Added</button>
         <button className='review-sort-buttons' onClick={() => {setOld(!isOld);setUnsorted(false);setHighLow(false);setLowHigh(false);setRecent(false)}}>Oldest-Recent Added</button>
-      </div>
-      
-      { isHighLow && <div>{reviewList(high_low)}</div> }
-      { isLowHigh && <div>{reviewList(low_high)}</div> }
-      { isUnsorted && <div>{reviewList(all_reviews)}</div> }
-      { isRecent && <div>{reviewList(recent)}</div> }
-      { isOld && <div>{reviewList(old)}</div> }
+        </div>
+        
+        { isHighLow && 
+        <div>{high_low_list}</div>
+        }
+        { isLowHigh && 
+        <div>{low_high_list}</div>
+        }
+        { isUnsorted && 
+        <div>{unsorted_list}</div>
+        }
+        { isRecent && 
+        <div>{recent_list}</div>
+        }
+        { isOld && 
+        <div>{old_list}</div>
+        }
     </div>
-  )
-}
-
+)}
+  
 export default Review;
--- src/components/TestSelect.jsx
+++ src/components/TestSelect.jsx
@@ -1,111 +1,115 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react'
+import '../App.css';
+import axios from 'axios'
 import Select from "react-select";
 import FetchResturauntName from './RestaurantDescription';
 import Modal from './Modal';
+import e from 'cors';
 import { Link } from 'react-router-dom';
 import Map from './Map';
+const queryParams = new URLSearchParams(window.location.search)
+    const user_id = queryParams.get("id");
+    console.log(user_id);
 
-const queryParams = new URLSearchParams(window.location.search);
-const user_id = queryParams.get("id");
-
-export default class TestSelect extends Component {
-  state = {
-    loading: true,
-    options_state: [],
-    names: "Loading...",
-    city: "Loading...",
-    phonenumber: "Loading...",
-    website: "Loading...",
-    id: "N/A",
-    reviews: [],
-    latitide: 39.710117443535886,
-    longitude: -75.11916101566422,
-    timestamp: "N/A",
-    userId: user_id
-  };
-
-  async componentDidMount() {
-    const answer = await axios.get("http://127.0.0.1:8000/api/restaurants");
-    const data = answer.data.restaurants;
-    const answer2 = await axios.get("http://127.0.0.1:8000/api/reviews");
-    const data2 = answer2.data.reviews;
+export default class TestSelect extends React.Component{
     
-    const review = [];
-    const combined_reviews = new Array(data2.length).fill([]);
-    const empty = new Array(5).fill("N/A");
-    const all_reviews = new Array();
-    for(let i = 0; i < data2.length; i++) {
-      review[i] = [
-        data2[i].id,
-        data2[i].review_title,
-        data2[i].review_text,
-        data2[i].review_total_score,
-        data2[i].restaurant_id,
-        data2[i].timestamp
-      ];
+    
+    state = {
+        loading : true,
+        options_state : [],
+        names : "Loading...",
+        city : "Loading...",
+        phonenumber: "Loading...",
+        website: "Loading...",
+        id : "N/A",
+        reviews : [],
+        latitide: 39.710117443535886,
+        longitude: -75.11916101566422,
+        timestamp: "N/A",
+        userId: "N/A"
+        
     }
 
-    for(let i = 0; i < data.length; i++) {
-      for(let k = 0; k < data2.length; k++) {
-        if (data[i].id === review[k][4]) {
-          combined_reviews[i].push(review[k]);
-        }
-      }
-      data[i].reviews = combined_reviews[i];
-    }
+    async componentDidMount() {
 
-    const options = data.map((restaurant) => ({
-      value: restaurant,
-      label: restaurant.name
-    }));
-
-    this.setState({
-      options_state: options,
-      loading: false
-    });   
-  }
-
-  render() {
-    return (
-      <div>
-        <Map
-          latitide={this.state.latitide}
-          longitude={this.state.longitude}
-          names={this.state.names}
-        />
-        <Select
-          className="select-class"
-          options={this.state.options_state}
-          onChange={e => {
-            const value = e.value;
-            this.setState({
-              city: value.city,
-              id: value.id,
-              names: value.name,
-              phonenumber: value.phone_number,
-              website: value.website,
-              reviews: value.reviews,
-              latitide: value.latitide,
-              longitude: value.longitude
-            });
-            if (value.latitide.length === 0) {
-              this.setState({
-                reviews: [["N/A", "N/A", "N/A", "N/A", "N/A"]]
-              });
+        const answer = await axios.get("http://127.0.0.1:8000/api/restaurants");
+        const data = await answer.data.restaurants
+        const answer2 = await axios.get("http://127.0.0.1:8000/api/reviews");
+        const data2 = await answer2.data.reviews
+       
+        
+        const review = new Array()
+        const combined_reviews = new Array()
+        const empty = new Array(5).fill("N/A")
+        const all_reviews = new Array()
+        for(let i = 0; i < data2.length;i++){
+            review[i] = [data2[i].id,data2[i].review_title,data2[i].review_text,data2[i].review_total_score,data2[i].restaurant_id,data2[i].timestamp]
+            combined_reviews[i] = [""]
+         }
+     
+        //Objects can't be stored as a value in the react select option value field. 
+        //To fix this I made an array called "object" that holds each part of the json data,
+        //which makes it able to be stored in the options.
+        const restaurant = new Array()
+        for(let i = 0; i < data.length;i++){
+           restaurant[i] = [data[i].city,data[i].id,data[i].name,data[i].phone_number,data[i].website,data[i].latitide,data[i].longitude]
+           for(let k = 0; k < data2.length;k++){
+            if ( restaurant[i][1] == review[k][4]){
+                combined_reviews[i].push(review[k])
             }
-          }}
-        />
-        <FetchResturauntName
-          user={this.state.userId}
-          id={this.state.id}
-          names={this.state.names}
-          city={this.state.city}
-          phonenumber={this.state.phonenumber}
-          website={this.state.website}
-          reviews={this.state.reviews}
-        />
-      </div> 
-    );
-  }
+
+           }
+           combined_reviews[i].shift()
+           restaurant[i].push(combined_reviews[i])
+        }
+           
+           
+        //Options has two components the value and the label, this for loop assigns the data we got
+        //from the axios request to the value and labels in the options.
+        const options = []; 
+        for(let i = 0; i < data.length;i++){
+            options[i] = {value : restaurant[i], label: data[i].name}
+        }
+        this.setState({options_state:options,loading:false,userId: user_id})   
+    }
+    
+    render() {
+        return (
+                <div>
+                    <Map latitide={this.state.latitide} longitude={this.state.longitude} names = {this.state.names} />
+                    <Select 
+                    className = "select-class"
+                    options={this.state.options_state} 
+                    onChange={e => {
+                        const response_len = e.value.length
+                        this.setState({
+                            city:e.value[0],
+                            id: e.value[1],
+                            names:e.value[2],
+                            phonenumber:e.value[3],
+                            website:e.value[4],
+                            reviews : e.value[7],
+                            latitide: e.value[5],
+                            longitude: e.value[6]
+                            })
+                           
+                        if(e.value[5].length == 0){
+                            this.setState({
+                                reviews : [["N/A","N/A","N/A","N/A","N/A"]]
+                            })
+                        }}}/>
+                
+                    <FetchResturauntName 
+                    user = {this.state.userId}
+                    id = {this.state.id}
+                    names = {this.state.names} 
+                    city = {this.state.city} 
+                    phonenumber = {this.state.phonenumber} 
+                    website = {this.state.website}
+                    reviews = {this.state.reviews}>
+                    </FetchResturauntName>
+                    
+                </div> 
+        );
+    }
 }
