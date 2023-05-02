c5446e6517389115675289eddd730220af4be32f
vianafLMCO <vianaf15@students.rowan.edu>
2023-05-02 19:50:02 -0400

    Testing Restaurant drop + des

--- src/components/RestaurantDescription.js
+++ src/components/RestaurantDescription.js
@@ -1,37 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../App.css';
-import { Link } from "react-router-dom"
-import Modal from "./Modal"
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Modal from './Modal';
 import Review from './Review';
 import ShowInfo from './ShowInfo';
 
-function FetchResturauntName(props){
-    const [isToggled, setIsToggled] = useState(true)
-    const [isposted, setIsposted] = useState(false)
-return (
-    <div>
-        <div className = "Description">
-            <p>Restaurant Name     : {props.names}</p>
-            <p>City                : {props.city}</p>
-            <p>Phone Number        : {props.phonenumber}</p>
-            <p>Website             : {props.website}</p> 
-            <div className='List-review-button' onClick={() => setIsToggled(!isToggled)}>
-                Reviews
-            </div> 
-            <div className='List-review-button' onClick={() => setIsposted(!isposted)}>
-               Post Reviews
-            </div> 
-        </div>
-        { isToggled && 
-        <Review 
-        reviews = {props.reviews}
-        /> }
-        { isposted && 
-        <ShowInfo rest_id = {props.id} user={props.user}></ShowInfo>} 
-    </div>
-)
-}
+function FetchResturauntName(props) {
+  const [isToggled, setIsToggled] = useState(true);
+  const [isPosted, setIsPosted] = useState(false);
 
+  return (
+    <div>
+      <div className="Description">
+        <p>Restaurant Name: {props.names}</p>
+        <p>City: {props.city}</p>
+        <p>Phone Number: {props.phonenumber}</p>
+        <p>Website: {props.website}</p> 
+        <div className="List-review-button" onClick={() => setIsToggled(!isToggled)}>
+          Reviews
+        </div> 
+        <div className="List-review-button" onClick={() => setIsPosted(!isPosted)}>
+          Post Reviews
+        </div> 
+      </div>
+      {isToggled && 
+        <Review reviews={props.reviews} />}
+      {isPosted && 
+        <ShowInfo rest_id={props.id} user={props.user} />}
+    </div>
+  );
+}
 
 export default FetchResturauntName;
--- src/components/RestaurantDropdown.jsx
+++ src/components/RestaurantDropdown.jsx
@@ -1,43 +1,40 @@
-import React, { useState, useEffect, handleChange } from 'react';
-import RestaurantSelect from "./RestaurantSelect"
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import RestaurantSelect from './RestaurantSelect';
+import axios from 'axios';
 
+function RestaurantDropdown({ placeholder }) {
+  const [restaurant, setRestaurant] = useState(null);
+  const [options, setOptions] = useState([]);
 
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/restaurants')
+      .then((response) => {
+        setOptions(response.data.map((entry) => ({
+          label: entry,
+          value: entry,
+        })));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-function RestaurantDropdown({placeholder, data}) {
-    const [restaurant, restaurantChange] = useState();
-    const [options, setOptions] = useState([]);
+  const handleChange = (selectedOption) => {
+    setRestaurant(selectedOption);
+  };
 
-    useEffect(() => {
-        axios.get("http://127.0.0.1:8000/api/restaurants")
-        .then((response)=> response.json())
-        .then((data) => {
-            setOptions(
-                data.map((entry) => {
-                    var singleObj = {};
-                    singleObj["label"] = entry;
-                    singleObj["value"] = entry;
-                    return singleObj;
-                })
-            );
-    
-        }).catch(error => {
-            console.error(error);
-        })
-    }, []);
-    
-    return (
-            <div className="restaurant-dropdown" style={{ padding: 0}}>
-                <RestaurantSelect
-                    value={restaurant}
-                    options={options}
-                    placeholder="Select Cheesesteak Jawn"
-                    dropdownPosition="top"
-                    onChange={handleChange}
-                    isSearchable
-                />
-            </div>
-        );
+  return (
+    <div className="restaurant-dropdown" style={{ padding: 0 }}>
+      <RestaurantSelect
+        value={restaurant}
+        options={options}
+        placeholder={placeholder}
+        dropdownPosition="top"
+        onChange={handleChange}
+        isSearchable
+      />
+    </div>
+  );
 }
 
 export default RestaurantDropdown;
